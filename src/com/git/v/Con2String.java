d0f653a8e6e848da33bcec14d4c3482ab70cfb10
Roshan <roshantom@gmail.com>
2023-01-10 13:22:25 +0530

    First Commit by Roshan

---
+++ src/com/git/v/Con2String.java
@@ -0,0 +1,13 @@
+package com.git.v;
+
+public class Con2String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String S1 = "Roshan ";
+		String S2 = "Tom";
+		System.out.println(S1.concat(S2));
+
+	}
+
+}
